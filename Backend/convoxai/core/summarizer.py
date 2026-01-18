8b6697ed09def61b5e7e330471c323b16835e201
Dhruv610ag <dhruv610agg@gmail.com>
2026-01-18 14:26:20 +0530

    Additional LLM Added

--- Backend/convoxai/core/summarizer.py
+++ Backend/convoxai/core/summarizer.py
@@ -1,25 +1,25 @@
 import warnings
-from typing import List, Literal, Annotated
-from pydantic import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
 from convoxai.utils.audio import transcribe_audio_simple
 from convoxai.core.prompts.templates import system_prompt
-from convoxai.config import GEMINI_API_KEY, GEMINI_MODEL_NAME, GEMINI_TEMPERATURE
+from convoxai.config import GEMINI_API_KEY, GEMINI_MODEL_NAME, GEMINI_TEMPERATURE, GROQ_API_KEY, GROQ_MODEL_NAME, GROQ_TEMPERATURE
+from convoxai.core.models import SummaryResponse
 
 warnings.filterwarnings("ignore")
-
-class SummaryResponse(BaseModel):
-    summary: Annotated[str, Field(..., description="Write the Concise call summary From the Transcript")]
-    duration_minutes: Annotated[int, Field(..., description="Calculate the Call duration in minutes")]
-    no_of_participants: Annotated[int, Field(..., description="Count the number of participants")]
-    key_aspects: Annotated[List[str], Field(..., description="List the key discussion points as bullet points")]
-    sentiment: Annotated[Literal["Positive", "Negative", "Neutral"], Field(..., description="Mention the sentiment of the call")]
 
 def create_llm():
     return ChatGoogleGenerativeAI(
         model=GEMINI_MODEL_NAME,
         api_key=GEMINI_API_KEY,
         temperature=GEMINI_TEMPERATURE
+    )
+
+def create_llm_2():
+    return ChatGroq(
+        model=GROQ_MODEL_NAME,
+        api_key=GROQ_API_KEY,
+        temperature=GROQ_TEMPERATURE
     )
 
 def generate_summary(audio_file_path: str | None = None) -> dict:
@@ -31,3 +31,4 @@ def generate_summary(audio_file_path: str | None = None) -> dict:
     final_prompt = system_prompt.format(transcript=transcript)
     response = st_llm.invoke(final_prompt)
     return response
+
