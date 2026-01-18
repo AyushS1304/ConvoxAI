ebb0445d8944db9598e1f9092ab3f0034f9caf2c
Dhruv610ag <dhruv610agg@gmail.com>
2026-01-18 14:28:27 +0530

    Groq-Mpdel,Api,Temp Added

--- Backend/convoxai/config.py
+++ Backend/convoxai/config.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+GROQ_API_KEY = os.getenv("Groq_API_Key")
+if not GROQ_API_KEY:
+    raise EnvironmentError("Groq_API_Key not found in .env file")
+
 GEMINI_API_KEY = os.getenv("Gemini_API_Key")
 if not GEMINI_API_KEY:
     raise EnvironmentError("Gemini_API_Key not found in .env file")
@@ -13,6 +17,9 @@ if not PINECONE_API_KEY:
 
 GEMINI_MODEL_NAME = "gemini-2.5-flash"
 GEMINI_TEMPERATURE = 0.7
+
+GROQ_MODEL_NAME = "qwen/qwen3-32b"
+GROQ_TEMPERATURE = 0.6
 
 WHISPER_MODEL_SIZE = "medium"
 
